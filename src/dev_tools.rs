5c3e34c0b155ce39febbaffab858b85abb9716a5
chris <cmorrison82z@proton.me>
2024-07-20 14:35:41 +0100

    Added flying camera to devtools

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,10 +1,51 @@
 //! Development tools for the game. This plugin is only enabled in dev builds.
 
-use bevy::{dev_tools::states::log_transitions, prelude::*};
+use bevy::{dev_tools::states::log_transitions, math::VectorSpace, prelude::*};
+use bevy_flycam::{FlyCam, NoCameraPlayerPlugin};
 
 use crate::screen::Screen;
 
+#[derive(Resource, Deref, DerefMut)]
+pub struct IsInDevMode(pub bool);
+
 pub(super) fn plugin(app: &mut App) {
     // Print state transitions in dev builds
-    app.add_systems(Update, log_transitions::<Screen>);
+    app.insert_resource(IsInDevMode(false))
+        .add_plugins(NoCameraPlayerPlugin)
+        .add_systems(Startup, setup)
+        .add_systems(
+            Update,
+            (
+                switch_to_dev_mode,
+                (change_cams).run_if(resource_changed::<IsInDevMode>),
+            ),
+        )
+        .add_systems(Update, log_transitions::<Screen>);
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            camera: Camera {
+                is_active: false,
+                ..Default::default()
+            },
+            transform: Transform::from_xyz(-10.0, 10.0, -10.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        FlyCam,
+    ));
+}
+
+fn switch_to_dev_mode(mut r_dmode: ResMut<IsInDevMode>, input: Res<ButtonInput<KeyCode>>) {
+    if input.just_pressed(KeyCode::KeyF) {
+        **r_dmode = !(**r_dmode);
+    }
+}
+
+fn change_cams(mut q_cams: Query<&mut Camera>) {
+    debug!("ran it");
+    q_cams.iter_mut().for_each(|mut c| {
+        c.is_active = !c.is_active;
+    })
 }
